f5f19663a66f787862eb82879648d7fbcc994634
minzykim <minpjh@gmail.com>
2018-09-21 01:17:35 +0900

    refactor: Refactor service to get data with test
    - Add method to get list id
    - Add method to get board labels
    - Add method to get board members

--- service.py
+++ service.py
@@ -9,7 +9,16 @@ class TrelloResourceService:
         self.__params = tokens
 
     def get_board_id(self, organ_name, board_name):
-        return self._get_id(self._make_board_url(organ_name), board_name)
+        return self._get_id(self._organization_board_url(organ_name), board_name)
+
+    def get_list_id(self, board_id, list_name):
+        return self._get_id(self._list_url(board_id), list_name)
+
+    def get_board_labels(self, board_id):
+        return self._request_to_get(self._labels_url(board_id), self.__params)
+
+    def get_board_members(self, board_id):
+        return self._request_to_get(self._members_url(board_id), self.__params)
         
     def _get_id(self, url, target):
         resp_data = self._request_to_get(url, self.__params)
@@ -23,8 +32,17 @@ class TrelloResourceService:
 
         raise ValueError('{} 존재하지 않습니다.'.format(target))
 
-    def _make_board_url(self, organ_name):
+    def _organization_board_url(self, organ_name):
         return os.path.join(self.base_url, 'organization', organ_name, 'boards')
+
+    def _list_url(self, board_id):
+        return os.path.join(self.base_url, 'boards', board_id, 'lists')
+
+    def _labels_url(self, board_id):
+        return os.path.join(self.base_url, 'boards', board_id, 'labels')
+
+    def _members_url(self, board_id):
+        return os.path.join(self.base_url, 'boards', board_id, 'members')
 
     def _request_to_get(self, url, params):
         return self._convert_json(requests.get(url, params=params).text)
--- tests/test_trello_resource_service.py
+++ tests/test_trello_resource_service.py
@@ -14,30 +14,66 @@ class TestTrelloResourceService(unittest.TestCase):
         self.service = TrelloResourceService(TRELLO_API, 'tokens')
         self.organ_name = 'test_organization'
         self.board_name = 'test_board'
+        self.list_name = 'test_list'
         self.url = 'http://test.url.com'
 
     @mock.patch('requests.get')
     def test_get_board_id(self, mock_req):
-        resp_data = [{'name': 'Sprint5 for Sep.', 'id': '1'}, {'name': self.board_name, 'id': '2'}]
-        mock_req.return_value = self._mock_resp(resp_data)
+        resp_data = [{'name': self.board_name + '1', 'id': '1'}, {'name': self.board_name, 'id': '2'}]
+        self._mock_request(mock_req, resp_data)
         self.assertEqual(self.service.get_board_id(self.organ_name, self.board_name), '2')
 
     @mock.patch('requests.get')
-    def test_not_exist_board_id(self, mock_req):
-        mock_req.return_value = self._mock_resp([])
+    def test_failed_not_exist_board_id(self, mock_req):
+        self._mock_request(mock_req, [])
         self.assertRaises(ValueError, self.service.get_board_id, self.organ_name, self.board_name)
 
     @mock.patch('requests.get')
-    def test_exist_multi_board_ids(self, mock_req):
+    def test_failed_exist_multi_board_ids(self, mock_req):
         resp_data = [{'name': self.board_name, 'id': '1'}, {'name': self.board_name, 'id': '2'}]
-        mock_req.return_value = self._mock_resp(resp_data)
+        self._mock_request(mock_req, resp_data)
         self.assertRaises(DuplicatedValueError, self.service.get_board_id, self.organ_name, self.board_name)
 
-    def _mock_resp(self, resp_data, status_code=200):
+    @mock.patch('requests.get')
+    def test_get_list_id(self, mock_req):
+        resp_data = [{'name': self.list_name + '1', 'id': '1'}, {'name': self.list_name, 'id': '2'}]
+        self._mock_request(mock_req, resp_data)
+        self.assertEqual(self.service.get_list_id('1', self.list_name), '2')
+
+    @mock.patch('requests.get')
+    def test_get_list_id(self, mock_req):
+        resp_data = [{'name': self.list_name + '1', 'id': '1'}, {'name': self.list_name, 'id': '2'}]
+        self._mock_request(mock_req, resp_data)
+        self.assertEqual(self.service.get_list_id('1', self.list_name), '2')
+
+    @mock.patch('requests.get')
+    def test_failed_not_exist_list_id(self, mock_req):
+        self._mock_request(mock_req, [])
+        self.assertRaises(ValueError, self.service.get_list_id, '1', self.list_name)
+
+    @mock.patch('requests.get')
+    def test_failed_exist_multi_list_ids(self, mock_req):
+        resp_data = [{'name': self.list_name, 'id': '1'}, {'name': self.list_name, 'id': '2'}]
+        self._mock_request(mock_req, resp_data)
+        self.assertRaises(DuplicatedValueError, self.service.get_list_id, '1', self.list_name)
+
+    @mock.patch('requests.get')
+    def test_get_board_labels(self, mock_req):
+        resp_data = ['labels_data']
+        self._mock_request(mock_req, resp_data)
+        self.assertEqual(self.service.get_board_labels('1'), resp_data)
+
+    @mock.patch('requests.get')
+    def test_get_board_members(self, mock_req):
+        resp_data = ['members_data']
+        self._mock_request(mock_req, resp_data)
+        self.assertEqual(self.service.get_board_members('1'), resp_data)
+
+    def _mock_request(self, mock_req, resp_data, status_code=200):
         mock_resp = mock.Mock()
         mock_resp.status_code = status_code
         mock_resp.text = json.dumps(resp_data)
-        return mock_resp
+        mock_req.return_value = mock_resp
 
 if __name__ == '__main__':
     unittest.main()
