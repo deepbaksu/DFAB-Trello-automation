bb125491e69ac5733062e1ba5290f9da41d8caf9
minzykim <minpjh@gmail.com>
2018-09-20 23:52:46 +0900

    refactor: Refactor service to get board id with test
    - Add DuplicatedValueError
    - Add method to get board id

--- service.py
+++ service.py
@@ -1,17 +1,38 @@
+import os
 import requests
 import json
+from common.exceptions import DuplicatedValueError
 
 class TrelloResourceService:
-    def __init__(self, base_url):
+    def __init__(self, base_url, tokens):
         self.base_url = base_url
+        self.__params = tokens
 
-    def request_to_get(self, url, params):
+    def get_board_id(self, organ_name, board_name):
+        return self._get_id(self._make_board_url(organ_name), board_name)
+        
+    def _get_id(self, url, target):
+        resp_data = self._request_to_get(url, self.__params)
+        data_ids = [data['id'] for data in resp_data if data['name'] == target]
+
+        if len(data_ids) == 1:
+            return data_ids[0]
+
+        if len(data_ids) > 1:
+            raise DuplicatedValueError('{} 2개 이상 존재합니다.'.format(target))
+
+        raise ValueError('{} 존재하지 않습니다.'.format(target))
+
+    def _make_board_url(self, organ_name):
+        return os.path.join(self.base_url, 'organization', organ_name, 'boards')
+
+    def _request_to_get(self, url, params):
         return self._convert_json(requests.get(url, params=params).text)
 
-    def request_to_post(self, url, params):
+    def _request_to_post(self, url, params):
         return self._convert_json(requests.post(url, params=params).text)
 
-    def request_to_put(self, url, params):
+    def _request_to_put(self, url, params):
         return self._convert_json(requests.put(url, params=params).text)
 
     def _convert_json(self, text_data):
--- tests/test_trello_resource_service.py
+++ tests/test_trello_resource_service.py
@@ -7,28 +7,31 @@ import mock
 import json
 from service import TrelloResourceService
 from lib.config import TRELLO_API
+from common.exceptions import DuplicatedValueError
 
 class TestTrelloResourceService(unittest.TestCase):
     def setUp(self):
-        self.service = TrelloResourceService(TRELLO_API)
-        self.resp_data = "[{'test': 'test'}]"
+        self.service = TrelloResourceService(TRELLO_API, 'tokens')
+        self.organ_name = 'test_organization'
+        self.board_name = 'test_board'
         self.url = 'http://test.url.com'
-        self.params = 'params'
 
     @mock.patch('requests.get')
-    def test_request_get_data(self, mock_req):
-        mock_req.return_value = self._mock_resp(self.resp_data)
-        self.assertEqual(self.service.request_to_get(self.url, self.params), self.resp_data)
+    def test_get_board_id(self, mock_req):
+        resp_data = [{'name': 'Sprint5 for Sep.', 'id': '1'}, {'name': self.board_name, 'id': '2'}]
+        mock_req.return_value = self._mock_resp(resp_data)
+        self.assertEqual(self.service.get_board_id(self.organ_name, self.board_name), '2')
 
-    @mock.patch('requests.post')
-    def test_create_list(self, mock_req):
-        mock_req.return_value = self._mock_resp(self.resp_data)
-        self.assertEqual(self.service.request_to_post(self.url, self.params), self.resp_data)
+    @mock.patch('requests.get')
+    def test_not_exist_board_id(self, mock_req):
+        mock_req.return_value = self._mock_resp([])
+        self.assertRaises(ValueError, self.service.get_board_id, self.organ_name, self.board_name)
 
-    @mock.patch('requests.put')
-    def test_create_list(self, mock_req):
-        mock_req.return_value = self._mock_resp(self.resp_data)
-        self.assertEqual(self.service.request_to_put(self.url, self.params), self.resp_data)
+    @mock.patch('requests.get')
+    def test_exist_multi_board_ids(self, mock_req):
+        resp_data = [{'name': self.board_name, 'id': '1'}, {'name': self.board_name, 'id': '2'}]
+        mock_req.return_value = self._mock_resp(resp_data)
+        self.assertRaises(DuplicatedValueError, self.service.get_board_id, self.organ_name, self.board_name)
 
     def _mock_resp(self, resp_data, status_code=200):
         mock_resp = mock.Mock()
