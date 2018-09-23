28ac13890a74e001d4c30e6bd68b60ddbf51fd42
minzykim <minpjh@gmail.com>
2018-09-24 00:49:19 +0900

    refactor: Refactor service to post data with test
    - Add post request to create archived list, create sprint board and move done cards
    - Refactor get_id method to return None if data is not exist
    - Add raise_for_status mock for handling HTTP error
    - Add raise_for_status to response

--- service.py
+++ service.py
@@ -1,7 +1,10 @@
-import os
-import requests
 import json
+import os
+
+import requests
+
 from common.exceptions import DuplicatedValueError
+
 
 class TrelloResourceService:
     def __init__(self, base_url, tokens):
@@ -19,39 +22,73 @@ class TrelloResourceService:
 
     def get_board_members(self, board_id):
         return self._request_to_get(self._members_url(board_id), self.__params)
-        
+
+    def create_archived_list(self, board_id, list_name, list_pos="bottom"):
+        list_id = self.get_list_id(board_id, list_name)
+        if list_id:
+            return list_id
+        params = self.__params.copy()
+        params['name'] = list_name
+        params['pos'] = list_pos
+        return self._request_to_post(self._list_url(board_id), params)['id']
+
+    def create_sprint_board(self, organ_name, board_name, default_list="false", board_pos="top", prefs_perm="org"):
+        params = self.__params.copy()
+        params['name'] = board_name
+        params['idOrganization'] = organ_name
+        params['defaultLists'] = default_list
+        params['pos'] = board_pos
+        params['prefs_permissionLevel'] = prefs_perm
+        return self._request_to_post(self._base_board_url(), params)['id']
+
+    def move_done_cards(self, board_id, from_list_id, to_list_id):
+        params = self.__params.copy()
+        params['idBoard'] = board_id
+        params['idList'] = to_list_id
+        return self._request_to_post(self._move_all_cards_url(from_list_id), params)
+
     def _get_id(self, url, target):
         resp_data = self._request_to_get(url, self.__params)
         data_ids = [data['id'] for data in resp_data if data['name'] == target]
 
         if len(data_ids) == 1:
             return data_ids[0]
-
         if len(data_ids) > 1:
             raise DuplicatedValueError('{} 2개 이상 존재합니다.'.format(target))
-
-        raise ValueError('{} 존재하지 않습니다.'.format(target))
+        None
 
     def _organization_board_url(self, organ_name):
         return os.path.join(self.base_url, 'organization', organ_name, 'boards')
 
+    def _base_board_url(self):
+        return os.path.join(self.base_url, 'boards')
+
     def _list_url(self, board_id):
-        return os.path.join(self.base_url, 'boards', board_id, 'lists')
+        return os.path.join(self._base_board_url(), board_id, 'lists')
+
+    def _move_all_cards_url(self, list_id):
+        return os.path.join(self.base_url, 'lists', list_id, 'moveAllCards')
 
     def _labels_url(self, board_id):
-        return os.path.join(self.base_url, 'boards', board_id, 'labels')
+        return os.path.join(self._base_board_url(), board_id, 'labels')
 
     def _members_url(self, board_id):
-        return os.path.join(self.base_url, 'boards', board_id, 'members')
+        return os.path.join(self._base_board_url(), board_id, 'members')
 
     def _request_to_get(self, url, params):
-        return self._convert_json(requests.get(url, params=params).text)
+        resp = requests.get(url, params=params)
+        resp.raise_for_status()
+        return self._convert_json(resp.text)
 
     def _request_to_post(self, url, params):
-        return self._convert_json(requests.post(url, params=params).text)
+        resp = requests.post(url, params=params)
+        resp.raise_for_status()
+        return self._convert_json(resp.text)
 
     def _request_to_put(self, url, params):
-        return self._convert_json(requests.put(url, params=params).text)
+        resp = requests.put(url, params=params)
+        resp.raise_for_status()
+        return self._convert_json(resp.text)
 
     def _convert_json(self, text_data):
         return json.loads(text_data)
--- tests/test_trello_resource_service.py
+++ tests/test_trello_resource_service.py
@@ -7,11 +7,12 @@ import mock
 import json
 from service import TrelloResourceService
 from lib.config import TRELLO_API
+from requests.exceptions import HTTPError
 from common.exceptions import DuplicatedValueError
 
 class TestTrelloResourceService(unittest.TestCase):
     def setUp(self):
-        self.service = TrelloResourceService(TRELLO_API, 'tokens')
+        self.service = TrelloResourceService(TRELLO_API, {'key': 'key', 'token': 'token'})
         self.organ_name = 'test_organization'
         self.board_name = 'test_board'
         self.list_name = 'test_list'
@@ -19,61 +20,151 @@ class TestTrelloResourceService(unittest.TestCase):
 
     @mock.patch('requests.get')
     def test_get_board_id(self, mock_req):
-        resp_data = [{'name': self.board_name + '1', 'id': '1'}, {'name': self.board_name, 'id': '2'}]
-        self._mock_request(mock_req, resp_data)
-        self.assertEqual(self.service.get_board_id(self.organ_name, self.board_name), '2')
+        resp_data = [{'name': self.board_name + '1', 'id': 'anotherBoardId'},
+                     {'name': self.board_name, 'id': 'boardId'}]
+        self._mock_request(mock_req=mock_req, resp_data=resp_data)
+        self.assertEqual(self.service.get_board_id(self.organ_name, self.board_name), resp_data[1]['id'])
 
     @mock.patch('requests.get')
-    def test_failed_not_exist_board_id(self, mock_req):
-        self._mock_request(mock_req, [])
-        self.assertRaises(ValueError, self.service.get_board_id, self.organ_name, self.board_name)
+    def test_failed_get_board_id(self, mock_req):
+        self._mock_request_http_error(mock_req)
+        self.assertRaises(HTTPError, self.service.get_board_id, self.organ_name, self.board_name)
+
+    @mock.patch('requests.get')
+    def test_not_exist_board_id(self, mock_req):
+        self._mock_request(mock_req=mock_req, resp_data=[])
+        self.assertIsNone(self.service.get_board_id(self.organ_name, self.board_name))
 
     @mock.patch('requests.get')
     def test_failed_exist_multi_board_ids(self, mock_req):
-        resp_data = [{'name': self.board_name, 'id': '1'}, {'name': self.board_name, 'id': '2'}]
-        self._mock_request(mock_req, resp_data)
+        resp_data = [{'name': self.board_name, 'id': 'boardId'}, {'name': self.board_name, 'id': 'boardId'}]
+        self._mock_request(mock_req=mock_req, resp_data=resp_data)
         self.assertRaises(DuplicatedValueError, self.service.get_board_id, self.organ_name, self.board_name)
 
     @mock.patch('requests.get')
     def test_get_list_id(self, mock_req):
-        resp_data = [{'name': self.list_name + '1', 'id': '1'}, {'name': self.list_name, 'id': '2'}]
-        self._mock_request(mock_req, resp_data)
-        self.assertEqual(self.service.get_list_id('1', self.list_name), '2')
+        resp_data = [{'name': self.list_name + '1', 'id': 'anotherListId'}, {'name': self.list_name, 'id': 'listId'}]
+        self._mock_request(mock_req=mock_req, resp_data=resp_data)
+        self.assertEqual(self.service.get_list_id('1', self.list_name), resp_data[1]['id'])
 
     @mock.patch('requests.get')
-    def test_get_list_id(self, mock_req):
-        resp_data = [{'name': self.list_name + '1', 'id': '1'}, {'name': self.list_name, 'id': '2'}]
-        self._mock_request(mock_req, resp_data)
-        self.assertEqual(self.service.get_list_id('1', self.list_name), '2')
+    def test_failed_get_list_id(self, mock_req):
+        self._mock_request_http_error(mock_req)
+        self.assertRaises(HTTPError, self.service.get_list_id, 'boardId', self.list_name)
 
     @mock.patch('requests.get')
-    def test_failed_not_exist_list_id(self, mock_req):
-        self._mock_request(mock_req, [])
-        self.assertRaises(ValueError, self.service.get_list_id, '1', self.list_name)
+    def test_not_exist_list_id(self, mock_req):
+        self._mock_request(mock_req=mock_req, resp_data=[])
+        self.assertIsNone(self.service.get_list_id('1', self.list_name))
 
     @mock.patch('requests.get')
     def test_failed_exist_multi_list_ids(self, mock_req):
-        resp_data = [{'name': self.list_name, 'id': '1'}, {'name': self.list_name, 'id': '2'}]
-        self._mock_request(mock_req, resp_data)
-        self.assertRaises(DuplicatedValueError, self.service.get_list_id, '1', self.list_name)
+        resp_data = [{'name': self.list_name, 'id': 'listId'}, {'name': self.list_name, 'id': 'listId'}]
+        self._mock_request(mock_req=mock_req, resp_data=resp_data)
+        self.assertRaises(DuplicatedValueError, self.service.get_list_id, 'boardId', self.list_name)
 
     @mock.patch('requests.get')
     def test_get_board_labels(self, mock_req):
         resp_data = ['labels_data']
-        self._mock_request(mock_req, resp_data)
-        self.assertEqual(self.service.get_board_labels('1'), resp_data)
+        self._mock_request(mock_req=mock_req, resp_data=resp_data)
+        self.assertEqual(self.service.get_board_labels('boardId'), resp_data)
+
+    @mock.patch('requests.get')
+    def test_failed_get_board_labels(self, mock_req):
+        self._mock_request_http_error(mock_req)
+        self.assertRaises(HTTPError, self.service.get_board_labels, 'boardId')
 
     @mock.patch('requests.get')
     def test_get_board_members(self, mock_req):
         resp_data = ['members_data']
-        self._mock_request(mock_req, resp_data)
-        self.assertEqual(self.service.get_board_members('1'), resp_data)
+        self._mock_request(mock_req=mock_req, resp_data=resp_data)
+        self.assertEqual(self.service.get_board_members('boardId'), resp_data)
 
-    def _mock_request(self, mock_req, resp_data, status_code=200):
+    @mock.patch('requests.get')
+    def test_failed_get_board_labels(self, mock_req):
+        self._mock_request_http_error(mock_req)
+        self.assertRaises(HTTPError, self.service.get_board_members, 'boardId')
+
+    @mock.patch('requests.post')
+    @mock.patch('requests.get')
+    def test_create_archived_list(self, mock_get_req, mock_post_req):
+        resp_data = {'name': self.list_name, 'id': 'listId'}
+        self._mock_request(mock_req=mock_get_req, resp_data=[])
+        self._mock_request(mock_req=mock_post_req, resp_data=resp_data)
+        self.assertEqual(self.service.create_archived_list('boardId', self.list_name), resp_data['id'])
+
+    @mock.patch('requests.get')
+    def test_exist_archived_list(self, mock_req):
+        resp_data = [{'name': self.list_name, 'id': 'listId'}]
+        self._mock_request(mock_req=mock_req, resp_data=resp_data)
+        self.assertEqual(self.service.create_archived_list('boardId', self.list_name), resp_data[0]['id'])
+
+    @mock.patch('requests.get')
+    def test_failed_exist_archived_list(self, mock_req):
+        self._mock_request_http_error(mock_req)
+        self.assertRaises(HTTPError, self.service.create_archived_list, 'boardId', self.list_name)
+
+    @mock.patch('requests.post')
+    @mock.patch('requests.get')
+    def test_failed_create_archived_list(self, mock_get_req, mock_post_req):
+        self._mock_request(mock_req=mock_get_req, resp_data=[])
+        self._mock_request_http_error(mock_post_req)
+        self.assertRaises(HTTPError, self.service.create_archived_list, 'boardId', self.list_name)
+
+    @mock.patch('requests.post')
+    @mock.patch('requests.get')
+    def test_create_sprint_board(self, mock_get_req, mock_post_req):
+        resp_data = {'name': self.board_name, 'id': 'boardId'}
+        self._mock_request(mock_req=mock_get_req, resp_data=[])
+        self._mock_request(mock_req=mock_post_req, resp_data=resp_data)
+        self.assertEqual(self.service.create_sprint_board(self.organ_name, self.board_name), resp_data['id'])
+
+    @mock.patch('requests.get')
+    def test_exist_sprint_board(self, mock_get_req):
+        resp_data = [{'name': self.list_name, 'id': 'boardId'}]
+        self._mock_request(mock_req=mock_get_req, resp_data=resp_data)
+        self.assertEqual(self.service.create_archived_list('boardId', self.list_name), resp_data[0]['id'])
+
+    @mock.patch('requests.get')
+    def test_failed_exist_sprint_board(self, mock_req):
+        self._mock_request_http_error(mock_req)
+        self.assertRaises(HTTPError, self.service.create_archived_list, '1', self.list_name)
+
+    @mock.patch('requests.post')
+    @mock.patch('requests.get')
+    def test_failed_create_sprint_board(self, mock_get_req, mock_post_req):
+        self._mock_request(mock_req=mock_get_req, resp_data=[])
+        self._mock_request_http_error(mock_post_req)
+        self.assertRaises(HTTPError, self.service.create_archived_list, 'boardId', self.list_name)
+
+    @mock.patch('requests.post')
+    def test_move_done_cards(self, mock_req):
+        resp_data = [{'idList': 'toListId'}]
+        self._mock_request(mock_req=mock_req, resp_data=resp_data)
+        self.assertEqual(self.service.move_done_cards('boardId', 'fromListId', 'toListId'), resp_data)
+
+    @mock.patch('requests.post')
+    def test_failed_move_done_cards(self, mock_req):
+        self._mock_request_http_error(mock_req)
+        self.assertRaises(HTTPError, self.service.move_done_cards, 'boardId', 'fromListId', 'toListId')
+
+    # Todo: test case가 너무 많은데 resource request 객체를 빼서 테스트 따로 할까 생각 중(500에 대한 error는 여기서 안해도 되자나 request 별로 한번씩만 하면 되자너)
+    # Todo: url에 대한 test 추가
+
+    def _mock_request(self, mock_req, resp_data="[]", status_code=200, raise_for_status=None):
         mock_resp = mock.Mock()
-        mock_resp.status_code = status_code
+        mock_resp.raise_for_status = mock.Mock()
+
+        if raise_for_status:
+            mock_resp.raise_for_status.side_effect = raise_for_status
+
         mock_resp.text = json.dumps(resp_data)
+        mock_resp.status_code = status_code
+
         mock_req.return_value = mock_resp
+
+    def _mock_request_http_error(self, mock_req):
+        self._mock_request(mock_req=mock_req, status_code=500, raise_for_status=HTTPError('Trello API is not working'))
 
 if __name__ == '__main__':
     unittest.main()
