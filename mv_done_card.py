415b833c3fb05a76d9358b12582fb711dd82379a
minzykim <minpjh@gmail.com>
2018-09-24 23:38:38 +0900

    feat: Create done cards archiver with test
    - Rename config constant values
    - Create DoneCardsArchiver with test
    - Reformatting
    - Add script for archiving done cards

--- mv_done_card.py
+++ mv_done_card.py
@@ -3,7 +3,7 @@
 """Move all cards from done list to archive list"""
 import sys
 sys.path.append('./lib')
-from lib.config import DOTTED_LINE, TEAM_INFO, TARGET_LIST
+from lib.config import DOTTED_LINE, TEAM_INFO, DONE_LIST_NAME
 from lib.logger import LOGGER
 from lib.utils import get_board_name, get_board_id, get_list_id, get_the_number_of_card, \
                       create_list, get_archive_name, move_all_cards, compute_sprint_n
@@ -20,7 +20,7 @@ for team in TEAM_INFO:
     bid = get_board_id(organ_name, board_name)
 
     if bid:
-        done_list_id = get_list_id(bid, TARGET_LIST)
+        done_list_id = get_list_id(bid, DONE_LIST_NAME)
         n_card = get_the_number_of_card(done_list_id)
 
         if done_list_id and n_card > 0:
