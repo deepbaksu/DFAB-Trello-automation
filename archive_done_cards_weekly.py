ad21f81d36689b47381fc8b1d0016c780fd2893f
minzykim <minpjh@gmail.com>
2018-10-09 22:34:25 +0900

    fix: Add date when archiving done cards

--- archive_done_cards_weekly.py
+++ archive_done_cards_weekly.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#_*_ coding: utf-8 _*_
+
 import datetime
 
 from card_archiver import DoneCardsArchiver
@@ -6,6 +7,8 @@ from lib.config import TEAM_INFO, DONE_LIST_NAME, TRELLO_API_URL
 from lib.query import QUERY
 from service import TrelloResourceService
 
+today = datetime.date.today()
+
 for team_info in TEAM_INFO.values():
-    archiver = DoneCardsArchiver(team_info, datetime.date.today())
-    archiver.archive_done_cards(TrelloResourceService(TRELLO_API_URL, QUERY), DONE_LIST_NAME)
+    archiver = DoneCardsArchiver(team_info, today)
+    archiver.archive_done_cards(TrelloResourceService(TRELLO_API_URL, QUERY), DONE_LIST_NAME, today)
